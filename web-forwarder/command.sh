eb751116f7a51b928f0fc1d3fc39b6aff0351bdf
Jordi Subirà Nieto <jordi.subira.nieto@gmail.com>
2023-03-21 10:36:43 +0100

    extend web-forwarder tools

--- web-forwarder/command.sh
+++ web-forwarder/command.sh
@@ -14,13 +14,16 @@ cmd_help() {
 
 cmd_start(){
     local_addr_list=("192.168.201.40:80" "192.168.201.40:50080")
-    endpoint_addr_list=("192.168.201.10:80" "192.168.201.40:50080")
+    endpoint_addr_list=("192.168.201.10:80" "192.168.201.10:50080")
+    acl_list=("./_example_config/acl_80.json" "./_example_config/acl_50080.json")
+
+    mkdir -p logs
 
     i=0
     for local_addr in ${local_addr_list[@]}
     do
-        echo "SCION forwarder listening at $local_addr; forwarding traffic to endpoint_addr: ${endpoint_addr_list[$i]}" 
-        nohup scion-web-forwarder --local-addr $local_addr --endpoint-addr ${endpoint_addr_list[$i]} --acl ./_example_config/acl.json &> /dev/null &
+        echo "SCION forwarder listening at $local_addr; forwarding traffic to endpoint_addr: ${endpoint_addr_list[$i]}, acl config: ${acl_list[$i]}" 
+        nohup scion-web-forwarder --local-addr $local_addr --endpoint-addr ${endpoint_addr_list[$i]} --acl ${acl_list[$i]} &> logs/$local_addr.out &
         (( i = i + 1))
     done
 }
